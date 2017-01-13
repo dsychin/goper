f219d05f33ac3eee3128962804c65b3d84f2a26c
ktat <ktat.is@gmail.com>
2017-01-13 23:55:28 +0900

    add option to output files. generate some functions automatically. remove default value of dsn and if dsn is null, show usage.

--- generator.go
+++ generator.go
@@ -1,25 +1,29 @@
 package goper
 
 import (
-	"io"
-	"os"
-	"fmt"
-	"strings"
 	"database/sql"
+	"fmt"
+	"io"
 	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
 var logger *log.Logger
 
-func init () {
+func init() {
 	logger = log.New(ColourStream{os.Stderr}, " [XXXX] ", log.LstdFlags)
 }
 
 // A SchemaWriter writes a set of tables to the writer denoted by Outfile
 type SchemaWriter struct {
-	PackageName string
-	Outfile     io.Writer
-	Tables      []*Table
+	PackageName    string
+	Outfile        io.Writer
+	OutfileFunc    io.Writer
+	RemoveFromType string
+	Tables         []*Table
 }
 
 // Write the schema
@@ -33,25 +37,119 @@ func (this *SchemaWriter) WriteSchema() {
 
 // Write an individual table
 func (this *SchemaWriter) WriteType(table *Table) {
-	fmt.Fprintf(this.Outfile, "type %s struct {\n", CamelCase(table.Name))
+	fmt.Fprintf(this.Outfile, "\ntype %s struct {\n", CamelCase(table.Name))
+	maxln := 0
 	for _, column := range table.Columns {
-		this.WriteField(&column)
+		if len(column.Name) > maxln {
+			maxln = len(column.Name)
+		}
+
 	}
+	var tableColumns []Column
+	for _, column := range table.Columns {
+		if column.DbType != "table" {
+			this.WriteField(&column, maxln)
+		} else {
+			tableColumns = append(tableColumns, column)
+		}
+	}
+	if len(tableColumns) > 0 {
+		fmt.Fprint(this.Outfile, "\n")
+	}
+	for _, column := range tableColumns {
+		this.WriteTableField(&column, maxln)
+	}
+
 	fmt.Fprintf(this.Outfile, "}\n")
-	fmt.Fprintf(this.Outfile,
+}
+
+func (this *SchemaWriter) WriteFunc(table *Table) {
+	ct := CamelCase(table.Name)
+	t := table.Name
+
+	hasId := regexp.MustCompile("_id$")
+
+	fmt.Fprintf(this.OutfileFunc,
 		`
-func (this *%s)Table() string {
+func (this %s) Table() string {
     return "%s"
 }
-`,
-		CamelCase(table.Name), table.Name)
 
+func (this %s) Get(id int) *%s {
+    row := %s{}
+    sql := "select * FROM %s WHERE id = ? LIMIT 1"
+    err := db.Get(&row, sql, id)
+    if err != nil {
+        if err.Error() == "sql: no rows in result set" {
+            return nil
+        } else {
+            panic(err)
+        }
+    }
+    return &row
+}
+`,
+		ct, t,
+		ct, ct, ct, t,
+	)
+	hasMultiId := 0
+	for _, table_column := range table.Columns {
+		if hasId.MatchString(table_column.Name) {
+			hasMultiId++
+			if hasMultiId > 1 {
+				return
+			}
+		}
+	}
+
+	for _, col := range table.Columns {
+		cn := col.Name
+		ccn := CamelCase(cn)
+		if hasId.MatchString(col.Name) {
+			fmt.Fprintf(this.OutfileFunc,
+				`
+func (this %s) GetBy%s(id int) *[]%s {
+    rows := []%s{}
+    sql := "select * FROM %s WHERE %s = ?"
+    err := db.Select(&rows, sql, id)
+    if err != nil {
+        if err.Error() == "sql: no rows in result set" {
+            return nil
+        } else {
+            panic(err)
+        }
+    }
+    return &rows
+}
+
+`,
+				ct, ccn, ct, ct, t, cn,
+			)
+		}
+	}
 }
 
 // Write an individual field
-func (this *SchemaWriter) WriteField(column *Column) {
-	fmt.Fprintf(this.Outfile, "\t%s %s `db:\"%s\"`\n",
-		CamelCase(column.Name), column.GoType(), column.Name)
+func (this *SchemaWriter) WriteField(column *Column, maxln int) {
+	maxlnstr := strconv.Itoa(maxln)
+
+	name := CamelCase(column.Name)
+	// name = regexp.MustCompile("(Id)$").ReplaceAllStringFunc(name, strings.ToUpper)
+	fmt.Fprintf(this.Outfile, "\t%-"+maxlnstr+"s %-10s `json:\"%s\" db:\"%s\"`\n",
+		name, column.GoType(), column.Name, column.Name)
+}
+
+// Write an individual table field
+func (this *SchemaWriter) WriteTableField(column *Column, maxln int) {
+	ccn := CamelCase(column.Name)
+	ccnType := ccn
+	if this.RemoveFromType != "" {
+		ccnType = regexp.MustCompile("^"+this.RemoveFromType).ReplaceAllString(ccn, "")
+	}
+	maxlnstr := strconv.Itoa(maxln)
+
+	fmt.Fprintf(this.Outfile, "\t%-"+maxlnstr+"s %-10s `json:\"%s\" db:\"%s\"`\n",
+		ccn, "*"+ccnType, column.Name, column.Name)
 }
 
 // Load the database schema into memory using introspection, populating .Tables
@@ -63,11 +161,11 @@ func (this *SchemaWriter) LoadSchema(driver string, schema string, db *sql.DB) e
 	if err != nil {
 		return err
 	}
+	fmt.Fprintf(this.Outfile, "package %s\n\n", this.PackageName)
 	for tables.Next() {
 		var ignored sql.NullString
 		t := new(Table)
 		tables.Scan(&t.Name)
-		this.Tables = append(this.Tables, t)
 		cols, err := db.Query(dialect.ListColumns(schema, *t))
 		if err != nil {
 			return err
@@ -84,8 +182,27 @@ func (this *SchemaWriter) LoadSchema(driver string, schema string, db *sql.DB) e
 				panic(err)
 			}
 			t.Columns = append(t.Columns, *c)
+			re := regexp.MustCompile("^(?P<table_name>.+)_id$")
+			if re.MatchString(c.Name) {
+				match := re.FindSubmatchIndex([]byte(c.Name))
+				var dst []byte
+				dst = re.ExpandString(dst, "$table_name", c.Name, match)
+				newColumn := Column{
+					Name:   string(dst),
+					DbType: "table",
+				}
+				t.Columns = append(t.Columns, newColumn)
+			}
 		}
+		this.Tables = append(this.Tables, t)
 		this.WriteType(t)
 	}
+	fmt.Fprintf(this.OutfileFunc, "package %s\n\n", this.PackageName)
+
+	// fmt.Fprintf(this.OutfileFunc, "import (\"fmt\")\n")
+	for _, table := range this.Tables {
+		this.WriteFunc(table)
+	}
+
 	return nil
 }
--- schema/download.go
+++ schema/download.go
@@ -1,17 +1,18 @@
 // schema -driver mysql|sqlite3|postgres -dsn dsn
 // Generate a set of a golang structs
 package main
-            
-import (    
-    "database/sql"
-    _ "github.com/go-sql-driver/mysql"
-    _ "github.com/mattn/go-sqlite3"
-	_ "github.com/lib/pq"
-    "flag"
+
+import (
+	"database/sql"
+	"flag"
 	"fmt"
-    "os"
-	"github.com/shanemhansen/goper"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/ktat/goper"
+	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
+	"io"
 	"log"
+	"os"
 )
 
 var dsn string
@@ -19,35 +20,71 @@ var driver string
 var schema string
 var logger *log.Logger
 var verbose bool
+var outfile string
+var pkg string
+var remove string
 
 func init() {
-	flag.StringVar(&dsn,"dsn", "user:password@tcp(127.0.0.1:3306)/main", "database dsn")
-    flag.StringVar(&driver, "driver", "mysql", "driver")
-    flag.StringVar(&schema, "schema", "main", "schema")
+	flag.StringVar(&dsn, "dsn", "", "database dsn like 'user:password@tcp(127.0.0.1:3306)/main'")
+	flag.StringVar(&driver, "driver", "mysql", "driver")
+	flag.StringVar(&schema, "schema", "main", "schema")
+	flag.StringVar(&outfile, "outfile_prefix", "", "prefix of file name ex: xxx specifys and xxx_func.go and xxx_schema.go will be generated")
+	flag.StringVar(&pkg, "package", "data", "package name")
+	flag.StringVar(&remove, "remove", "", "remove string from head of type name")
 	flag.BoolVar(&verbose, "verbose", false, "Print debugging")
-    flag.Parse()
+	flag.Parse()
 
 	logger = log.New(goper.ColourStream{os.Stderr}, " [XXXX] ", log.LstdFlags)
+
+	if dsn == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
 }
 
 func main() {
 	conn, err := sql.Open(driver, dsn)
-    if err != nil {
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-        panic(err)
-    }
+		panic(err)
+	}
 	err = conn.Ping()
-    if err != nil {
+	if err != nil {
 		logger.Panic(err)
-    } else if verbose {
+	} else if verbose {
 		logger.Printf("Ping Worked\n")
 	}
-    os.Stdout.Write([]byte("package data\n"))
-    writer := &goper.SchemaWriter{Outfile: os.Stdout, PackageName: "data"}
+	var outSchema io.Writer
+	var outFunc io.Writer
+	if outfile == "" {
+		outSchema = os.Stdout
+		outFunc = os.Stdout
+	} else {
+		f, err := os.Create(outfile + "_schema.go")
+		if err != nil {
+			panic(err)
+		}
+		f2, err2 := os.Create(outfile + "_func.go")
+		if err2 != nil {
+			panic(err2)
+		}
+		outSchema = f
+		outFunc = f2
+
+		defer f.Close()
+		defer f2.Close()
+	}
+
+	writer := &goper.SchemaWriter{
+		Outfile:        outSchema,
+		OutfileFunc:    outFunc,
+		PackageName:    pkg,
+		RemoveFromType: remove,
+	}
 	//os.Stdout.Write([]byte(schema))
 	err = writer.LoadSchema(driver, schema, conn)
-    if err != nil {
+	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-        panic(err)
-    }
+		panic(err)
+	}
 }
